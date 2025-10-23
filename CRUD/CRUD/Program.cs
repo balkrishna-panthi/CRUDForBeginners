bcdb309842a7219b06386b4d7bcb232d0df11ba5
Balkrishna Panthi <balkrishnapanthi22@gmail.com>
2025-10-23 13:21:23 +0200

    added student class

--- CRUD/CRUD/Program.cs
+++ CRUD/CRUD/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. View Student");
+            string choice = Console.ReadLine(); // string is a data type
+            List<Student> students = new List<Student>();
+            
+
+            switch (choice)
+            {
+                case "1":
+                    Student student1 = new Student();
+                    Student student2 = new Student();
+                    student1.Name = "ABC"; // assigning
+                    student1.Id = 101;
+                    Console.WriteLine(student1.Name + " " + student1.Id); //accessing the value of Name attribute
+
+                    student2.Id = 102;
+                    student2.Name = "XYZ"; // assigning
+                    Console.WriteLine(student2.Name + " " + student2.Id); //accessing the value of Name attribute
+                    students.Add(student1);
+                    students.Add(student2);
+
+                    for(int i =0; i < 2; i++)
+                    {
+                        Console.WriteLine(students[i].Id + " " + students[i].Name);
+                    }
+
+
+                    break;
+                case "2":
+                    Console.WriteLine("The choice is : " + choice);
+                    break;
+                default:
+                    break;
+            }
         }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //study getter setter in c#
+        public int Age { get; set; } 
     }
 }
